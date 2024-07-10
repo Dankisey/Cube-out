2b21f6313c63d5e7459c7e06ad0d1df8bd27530d
Dankisey <91971084+Dankisey@users.noreply.github.com>
2024-07-10 22:46:18 +0300

    Music added

---
+++ Assets/Scripts/Level/MusicController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+namespace Game.Level
+{
+    public class MusicController : MonoBehaviour
+    {
+        private const string IsMutedInSettings = nameof(IsMutedInSettings);
+
+        [SerializeField] private BackgroundChangingObserver _backgroundChangingObserver;
+        [SerializeField] private AudioSource _audioSource;
+
+        public bool IsMuted => PlayerPrefs.GetInt(IsMutedInSettings) == (int)BoolEnum.True;
+
+        private void Awake()
+        {
+            if (PlayerPrefs.HasKey(IsMutedInSettings))
+            {
+                if (IsMuted)
+                    _audioSource.mute = true;
+                else
+                    _audioSource.mute = false;
+            }
+            else
+            {
+                PlayerPrefs.SetInt(IsMutedInSettings, (int)BoolEnum.False);
+            }
+        }
+
+        private void OnEnable()
+        {
+            _backgroundChangingObserver.IsReturnedToApp += OnReturnedToApp;
+            _backgroundChangingObserver.IsRunningInBackground += OnRunningInBackground;
+        }
+
+        private void OnDisable()
+        {
+            _backgroundChangingObserver.IsReturnedToApp -= OnReturnedToApp;
+            _backgroundChangingObserver.IsRunningInBackground -= OnRunningInBackground;
+        }
+
+        public void MuteAll()
+        {
+            PlayerPrefs.SetInt(IsMutedInSettings, (int)BoolEnum.True);
+            Mute();
+        }
+
+        public void UnmuteAll()
+        {
+            PlayerPrefs.SetInt(IsMutedInSettings, (int)BoolEnum.False);
+            Unmute();
+        }
+
+        private void Mute()
+        {
+            _audioSource.mute = true;
+        }
+
+        private void Unmute()
+        {
+            _audioSource.mute = false;
+        }
+
+        private void OnReturnedToApp()
+        {
+            if (IsMuted == false)
+                Unmute();
+        }
+
+        private void OnRunningInBackground()
+        {
+            if (IsMuted)
+                return;
+
+            Mute();
+        }
+
+        private enum BoolEnum
+        {
+            False = 0,
+            True = 1
+        }
+    }
+}
